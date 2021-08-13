df165d5cd257283d5e4a6b0990726a15026c6f8c
trpark <sdf7575@naver.com>
2021-08-13 12:18:15 +0900

    Time: 74 ms (14.30%), Space: 124.1 MB (10.86%) - LeetHub

---
+++ lru-cache/lru-cache.java
@@ -0,0 +1,26 @@
+
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+
+public class LRUCache extends LinkedHashMap<Integer, Integer> {
+
+  int capacity;
+
+  public LRUCache(int capacity) {
+    super(capacity, 0.75F, true);
+    this.capacity = capacity;
+  }
+
+  public int get(int key) {
+    return super.getOrDefault(key, - 1);
+  }
+
+  public void put(int key, int value) {
+    super.put(key, value);
+  }
+
+  @Override
+  protected boolean removeEldestEntry(Entry<Integer, Integer> eldest) {
+    return size() > capacity;
+  }
+}
