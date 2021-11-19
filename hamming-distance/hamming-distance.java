66cea66e5a361a46e73955b9fb10fe548ac62c0e
trpark <sdf7575@naver.com>
2021-11-19 14:31:13 +0900

    Time: 0 ms (100.00%), Space: 35.7 MB (83.94%) - LeetHub

--- hamming-distance/hamming-distance.java
+++ hamming-distance/hamming-distance.java
@@ -1,11 +1,5 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        int differ = x^y;
-        int hammingDistance = 0;
-        while (differ > 0) {
-            hammingDistance += differ&1;
-            differ >>= 1;
-        }
-        return hammingDistance;
+        return Integer.bitCount(x^y);
     }
 }
