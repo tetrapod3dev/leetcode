5297523f76b0fb800390d57a5848ef968d07ae95
Tetrapod 3 Dev <tetrapod3dev@gmail.com>
2023-11-29 01:48:42 +0000

    Time: 0 ms (100%), Space: 39.06 MB (86.89%) - LeetHub

---
+++ number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,11 @@
+class Solution {
+    // You need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            count += n & 1;
+            n >>>= 1;
+        }
+        return count;
+    }
+}
